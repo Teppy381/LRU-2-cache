697f9044c7b3fbeba92a5fe998cbb79ca2e76c00
VoroninMatvey <126752940+VoroninMatvey@users.noreply.github.com>
2023-05-10 19:42:28 +0300

    Add files via upload

---
+++ LRU-1/main_LRU1.cpp
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+struct node {
+
+    struct node *next;
+    struct node *prev;
+    unsigned long long val;
+
+};
+
+struct Hashtable {
+
+    struct node **lists_arr;
+    struct node *list_head;
+    struct node *list_tail;
+
+    unsigned long long size;
+    unsigned long long inserts;
+
+};
+
+
+ struct Hashtable *HashtableInit(unsigned long long size) {
+
+    struct Hashtable *HashT = (struct Hashtable *)calloc(1, sizeof(struct Hashtable));
+    HashT->lists_arr = (struct node **)calloc(size, sizeof(struct node *));
+
+    assert(HashT != NULL);
+    assert(HashT->lists_arr != NULL);
+
+    HashT->size = size;
+    HashT->inserts = 0;
+
+    return HashT;
+
+}
+
+int HashtableInsert(struct Hashtable *HashT, int m, unsigned long long val) {
+
+    int hits = 0;
+    unsigned long long hash1 = val;
+    int capacity = m;
+
+    if(HashT->lists_arr[hash1] == NULL) {
+
+        struct node *node = (struct node *)calloc(1, sizeof(struct node));
+        assert(node != NULL);
+        node->val = val;
+
+        if(HashT->inserts >= capacity) {
+
+            unsigned long long hash2 = HashT->list_tail->val;
+            HashT->lists_arr[hash2] = NULL;
+            HashT->list_tail = HashT->list_tail->prev;
+            free(HashT->list_tail->next);
+            HashT->list_tail->next = NULL;
+
+        }
+
+        HashT->lists_arr[hash1] = node;
+        node->next = HashT->list_head;
+
+        if(HashT->inserts == 0) {
+
+            HashT->list_head = node;
+            HashT->list_tail = node;
+
+        }
+
+        else {
+
+            HashT->list_head->prev = node;
+            HashT->list_head = node;
+
+        }
+
+        HashT->inserts += 1;
+
+    }
+
+    else {
+
+        if(HashT->lists_arr[hash1] != HashT->list_head) {
+
+            HashT->lists_arr[hash1]->prev->next = HashT->lists_arr[hash1]->next;
+
+
+            if(HashT->lists_arr[hash1]->next != NULL) {
+                HashT->lists_arr[hash1]->next->prev = HashT->lists_arr[hash1]->prev;
+            }
+            else {
+                HashT->list_tail = HashT->list_tail->prev;
+            }
+
+            HashT->lists_arr[hash1]->next = HashT->list_head;
+            HashT->lists_arr[hash1]->prev = NULL;
+            HashT->list_head->prev = HashT->lists_arr[hash1];
+            HashT->list_head = HashT->lists_arr[hash1];
+
+        }
+        hits = 1;
+    }
+
+    return hits;
+}
+
+void DeleteList(struct node *top) {
+
+    struct node *cur_node = top;
+    struct node *next_node = top;
+
+    while(cur_node->next != NULL) {
+
+        next_node = cur_node->next;
+        free(cur_node);
+        cur_node = next_node;
+    }
+
+    free(cur_node);
+}
+
+void DeleteHashtable(struct Hashtable *HashT) {
+
+    DeleteList(HashT->list_head);
+    free(HashT->lists_arr);
+    free(HashT);
+
+}
+
+int RequestInput(struct Hashtable *HashT, int m, int n) {
+
+    unsigned long long request = 0;
+    int res = 0;
+    int hits = 0;
+    int i = 0;
+
+    for(i = 0; i < n; ++i) {
+
+        res = scanf("%llu", &request);
+        assert(res);
+        hits = hits + HashtableInsert(HashT, m, request);
+
+    }
+    return hits;
+
+}
+
+int main() {
+
+    int res = 0;
+    int m = 0;
+    int n = 0;
+    int hits = 0;
+    struct Hashtable *HashT = NULL;
+    unsigned long long Hash_size = 500000;
+
+    res = scanf("%d", &m);
+    assert(res);
+    res = scanf("%d", &n);
+    assert(res);
+
+    HashT = HashtableInit(Hash_size);
+
+    hits = RequestInput(HashT, m, n);
+
+    printf("%d", hits);
+
+    DeleteHashtable(HashT);
+
+    return 0;
+
+}
